892513cad845dc2cb46a4b400edb5a758a60bf52
Diramix <39developer@diram1x.ru>
2026-05-31 15:41:34 +0300

    feat: Allow connect to dev server for anybody

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+	allowedDevOrigins: ["*.*.*.*"],
 	output: "export",
 	images: {
 		unoptimized: true,
